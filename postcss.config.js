ad4b9a68c9dd77b40548f8c3d3957bd8f3b3d2d8
Tony DeCarlo <anthony.decarlo@t1cg.com>
2018-12-21 13:44:26 -0500

    Updated to be react celeb site

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-
-const postcssConfig = {
-  plugins: [
-    require('postcss-import')({addDependencyTo: webpack}),
-    require('precss'),
-    require('autoprefixer')
-  ]
-};
